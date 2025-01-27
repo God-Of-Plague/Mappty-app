75433753e1d0b935ee16449cb0d41c64c7fd3389
LonelyCigarette <fftoken183@gmail.com>
2025-01-27 19:15:21 +0530

    basics to find the coordinates

--- script.js
+++ script.js
@@ -11,4 +11,19 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+// to catch the current location coordinates we use geolocation as shown in the below
+// navigatior.geolocation.getCurrentPosition will take 2 callback functions as arguments.
+// 1st argument executed when the location is spotted. means true for location.
+// 2nd location executed when there is an error or unable to spot the location. means false.
 
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    alert('location spotted! charge everyone');
+    const { latitude } = position.coords;
+    const { longitude } = position.coords;
+    console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+  },
+  function () {
+    alert('not able to get the location');
+  }
+);
