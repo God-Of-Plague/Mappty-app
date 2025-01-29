8008b6e3ce26988fa16dec7b76689adddadcb324
LonelyCigarette <fftoken183@gmail.com>
2025-01-29 21:52:13 +0530

    workout and it's child classes are created

--- script.js
+++ script.js
@@ -11,14 +11,52 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-/* According to the image first when the map loads constructor and getposition functions should be called.
-  so when the object is created getposition function is called immediatley
-  so the constructor should call the getposition function inside it.
-  so execution starts from constructor and then getposition
-*/
+// As in the picture we will create a parent class workout and child classes running and cycling
+
+class Workout {
+  // we will calculate the date when the object is created.
+  // we will need some identifier to know what object is what. so we can use id which is unique to know about the object
+  date = new Date();
+  // here the logic we use for id is create a date and last 10 digits of it
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; // [lat, lng]
+    this.distance = distance; // in km
+    this.duration = duration; // in min
+  }
+}
+
+// child classes creation and their methods are called in constructors
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
 
 class App {
-  // the variables defined outside are now inside
   #map;
   #mapEvent;
 
@@ -27,20 +65,13 @@ class App {
     this._getPosition();
 
     // Attach event handlers
-    // here we pass this as argument and it represents the current event object.
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
   }
 
-  /* getposition will get the user's location using navigation.geolocation. but it will call two functions once when we successfully gets the location, and one if not  */
   _getPosition() {
     if (navigator.geolocation)
       navigator.geolocation.getCurrentPosition(
-        // when we call it normally like this._loadmap(),it is called as function
-        // so location status is true then it should consider them as a regular function call not as a method call.
-        // so we should call it with as below so that we can get the object passed on as this. otherwise it this keyword is undefined in loadmap
-        // then we will face error in loadmap as it try to acess the object throuth this.#map and etc.,
-
         this._loadMap.bind(this),
         function () {
           alert('Could not get your position');
@@ -61,33 +92,23 @@ class App {
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(this.#map);
 
-    /* 
-    this.#map.on('click', function (mapE) {...}), 
-    the this inside the function callback refers to the global object (or undefined in strict mode) rather than the instance of the App class.
-    Arrow functions do not have their own this context, so this will refer to the enclosing context (in this case, the instance of App).
-    */
     this.#map.on('click', mapE => this._showForm(mapE));
   }
 
-  // showform basically makes the form revel itself by removing the hidden class in it.so we will write the code which is in map.on listner before to this showform function
   _showForm(mapE) {
-    this.#mapEvent = mapE; // Now `this` correctly refers to the `App` instance
+    this.#mapEvent = mapE;
     form.classList.remove('hidden');
     inputDistance.focus();
   }
 
-  // toggle event basically contain toggling of form to hidden and viceversa.so the code in inputtype listner is written here
   _toggleElevationField() {
     inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
     inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
   }
 
-  // so when we call it with this._newWorkout.bind(this). where this is eventlistner object. so this is copied into e.
-  // so e is event object
   _newWorkout(e) {
     e.preventDefault();
 
-    // making all the input values empty
     inputDistance.value =
       inputDuration.value =
       inputCadence.value =
