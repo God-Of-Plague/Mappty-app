533cab6825a9c181ada78b72c45b780188e95814
LonelyCigarette <fftoken183@gmail.com>
2025-01-29 14:32:40 +0530

    form option is added

--- script.js
+++ script.js
@@ -11,11 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-// to catch the current location coordinates we use geolocation as shown in the below
-// navigatior.geolocation.getCurrentPosition will take 2 callback functions as arguments.
-// 1st argument executed when the location is spotted. means true for location.
-// 2nd location executed when there is an error or unable to spot the location. means false.
-
+// these variables are declared globally.
+let map, mapevent;
 navigator.geolocation.getCurrentPosition(
   function (position) {
     const { latitude } = position.coords;
@@ -23,38 +20,58 @@ navigator.geolocation.getCurrentPosition(
     console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
 
     const coords = [latitude, longitude];
-    const map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
 
-    // to add a marker on the map whenever we click on the map. we use eventlisterners which are provided by the api.
-    map.on('click', function (mapevent) {
-      console.log(mapevent);
-      // when we click on mapevent is generated
-      const { lat, lng } = mapevent.latlng; // variable separation using objects
-      console.log(lat, lng);
-      // this code will mark the map by using the latitude and longitude.
-      // all popup options are from the documentation. check if you want to see.
-      // all of those options determine the popup appear on top of location symbol. and class name is added to that as mentioned.[check it's styles in css]
-      L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-          L.popup({
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-          })
-        )
-        .setPopupContent('workout')
-        .openPopup();
+    map.on('click', function (mapE) {
+      mapevent = mapE;
+      form.classList.remove('hidden');
+      inputDistance.focus();
     });
   },
   function () {
     alert('not able to get the location');
   }
 );
+
+// form is the element which will display when clicked on map
+// check html for form class.
+// so when the button is clicked form appeared. and after enetering value then clicking enter then the form will submit.
+// so when form submits we will do the follwing which we done in map.on previously
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  // making all the input values empty
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+
+  console.log(mapevent);
+  const { lat, lng } = mapevent.latlng;
+  console.log(lat, lng);
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('workout')
+    .openPopup();
+});
+
+// when we change the running to cycling then we have to change elevation to cadience in the form
+inputType.addEventListener('change', function(){
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
